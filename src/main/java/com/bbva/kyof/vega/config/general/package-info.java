54f7b1d8d9018eff1ad3d20dd3e63bfdbd5211a3
jorgediezrujas@gmail.com <jorge.diez.contractor@bbva.com>
2019-04-04 15:33:37 +0200

    1.5.1 upload

--- src/main/java/com/bbva/kyof/vega/config/general/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.bbva.com/vega/config", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.bbva.kyof.vega.config.general;
--- src/main/java/com/bbva/kyof/vega/config/privkey/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.bbva.com/vega/priv_key", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.bbva.kyof.vega.config.privkey;
